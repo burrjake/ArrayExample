fe171629242cdf53d6632b7771b38a095eb635e2
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-09 09:01:16 -0600

    add name array and data array

--- ArrayExample.vb
+++ ArrayExample.vb
@@ -4,4 +4,25 @@
 
     End Sub
 
+    Sub SimpleArray()
+        Dim names(5) As String
+
+        names(0) = "Bob"
+        names(3) = "Jimmy"
+        Console.ReadLine()
+
+        For i = LBound(names) To UBound(names)
+
+        Next
+    End Sub
+
+    Sub Multidimensional()
+        Dim dataArray(3, 5) As Integer
+
+        dataArray(0, 0) = 5
+        dataArray(1, 0) = 7
+
+
+    End Sub
+
 End Module
